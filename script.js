6811d8dc6b650c2eeb9aba5ee4dd592b66a52dad
Isuru-sasanga <kolamba-se19038@stu.kln.ac.lk>
2023-08-11 01:02:41 +0530

    add about and education parts

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ MenuBtn.addEventListener('click', function(e){
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 }) 
+
+
+function toggleContent(contentId) {
+    
+    var content = document.getElementById(contentId);
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
+
